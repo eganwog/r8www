05baecb1ec4ce0338566ce00df3bfe142c839785
eganwog <eganwog@gmail.com>
2011-11-03 10:03:04 -0700

    Added CSS file

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Robotron</title>
   </head>
+  <link rel="stylesheet" type="text/css" href="index.css">
 <body>
   <h1>Robotron</h1>
 <?php
